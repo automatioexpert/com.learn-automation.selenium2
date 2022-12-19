09f57341390657f7eaeb2121cc29a82c196bbcee
Valmiki Vishwakarma <you@example.com>
2022-12-19 17:11:52 +0530

    add.

---
+++ com.learn-automation.selenium2/src/test/java/testcases/dataDrivenTestCases/DemoClass.java
@@ -0,0 +1,13 @@
+package testcases.dataDrivenTestCases;
+
+public class DemoClass {
+
+	public static void main(String[] args) 
+	{
+	
+		
+		System.out.println(System.getProperty("user.dir"));
+
+	}
+
+}
